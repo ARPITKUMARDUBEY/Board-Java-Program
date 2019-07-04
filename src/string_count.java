454db9872a46f1a225c10fee49893490fbd9f043
ARPIT <arpitdubey4462@gmail.com>
2019-07-03 17:20:00 -0700

    02/07/2019

--- src/string_count.java
+++ src/string_count.java
@@ -4,7 +4,7 @@ public class string_count {
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        int count=0,k=0,flag=0;
+        int count=0,k=0,flag=0,counter=0;
         System.out.print("PLEASE ENTER THE DATA  =  ");
         String data= sc.next();
         for(int i=0;i<data.length();i++)
@@ -27,12 +27,16 @@ public class string_count {
                 else
                 {
                      flag=1;
+
                 }
             }
             if(flag==1)
             {
-                System.out.print(data.charAt(i)+"---->"+count);
+                System.out.print(data.charAt(i)+"---->"+count+"\n");
+                counter++;
             }
         }
+        System.out.println("total unique character are = "+counter);
     }
+
 }
